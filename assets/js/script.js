5f7388c8d0458f3058aa2fd2801dae4de5124b99
CraigJensen <cmjensen82@gmail.com>
2021-12-12 08:56:05 -0600

    Added variables and click listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
-var getWeatherData = function () {};
-var apiUrl =
-  'https://api.openweathermap.org/data/2.5/weather?q=austin&appid=ba973368929878e06f2318c9fa6bd307';
+var searchBtnEl = document.querySelector('.search-btn');
+var searchFieldEl = document.querySelector('#searchField');
 
-fetch(apiUrl).then(function (response) {
-  if (response.ok) {
-    response.json().then(function (data) {});
-  }
-});
+var getWeatherData = function (data) {
+  var apiUrl =
+    'https://api.openweathermap.org/data/2.5/weather?q=austin&appid=ba973368929878e06f2318c9fa6bd307';
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {});
+    }
+  });
+};
+
+searchBtnEl.addEventListener('click', getWeatherData);
