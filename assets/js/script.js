52cfed18d61db5a5ad2fc932b293c0410d021a37
CraigJensen <cmjensen82@gmail.com>
2021-12-13 15:43:05 -0600

    changed layout of variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var searchBtnEl = document.querySelector('.search-btn');
 var searchFieldEl = document.querySelector('#searchField');
 
+// Weather Element Data
 var currentWeatherEl = document.querySelector('.current-weather');
 var cityDateEl = document.querySelector('.city-date');
 var cityTempEl = document.querySelector('.temp');
@@ -9,37 +10,38 @@ var cityFeelsEl = document.querySelector('.feels-like');
 var cityHumidityEl = document.querySelector('.humidity');
 var weatherDescriptionEl = document.querySelector('.weather');
 
+// 5 Day Forecast Data
 var weatherCardEl0 = document.querySelector('#weatherCard0');
-var weatherCardEl1 = document.querySelector('#weatherCard1');
-var weatherCardEl2 = document.querySelector('#weatherCard2');
-var weatherCardEl3 = document.querySelector('#weatherCard3');
-var weatherCardEl4 = document.querySelector('#weatherCard4');
-
 var cardTempEl0 = document.querySelector('.temp0');
 var cardRealTempEl0 = document.querySelector('.real-temp0');
 var cardHumidityEl0 = document.querySelector('.humidity0');
 var cardWeatherEl0 = document.querySelector('.weather0');
 
+var weatherCardEl1 = document.querySelector('#weatherCard1');
 var cardTempEl1 = document.querySelector('.temp1');
 var cardRealTempEl1 = document.querySelector('.real-temp1');
 var cardHumidityEl1 = document.querySelector('.humidity1');
 var cardWeatherEl1 = document.querySelector('.weather1');
 
+var weatherCardEl2 = document.querySelector('#weatherCard2');
 var cardTempEl2 = document.querySelector('.temp2');
 var cardRealTempEl2 = document.querySelector('.real-temp2');
 var cardHumidityEl2 = document.querySelector('.humidity2');
 var cardWeatherEl2 = document.querySelector('.weather2');
 
+var weatherCardEl3 = document.querySelector('#weatherCard3');
 var cardTempEl3 = document.querySelector('.temp3');
 var cardRealTempEl3 = document.querySelector('.real-temp3');
 var cardHumidityEl3 = document.querySelector('.humidity3');
 var cardWeatherEl3 = document.querySelector('.weather3');
 
+var weatherCardEl4 = document.querySelector('#weatherCard4');
 var cardTempEl4 = document.querySelector('.temp4');
 var cardRealTempEl4 = document.querySelector('.real-temp4');
 var cardHumidityEl4 = document.querySelector('.humidity4');
 var cardWeatherEl4 = document.querySelector('.weather4');
 
+// Fetch Data
 var searchAsideEl = document.querySelector('.city-search');
 var fetchId = '&appid=ba973368929878e06f2318c9fa6bd307';
 
@@ -71,6 +73,7 @@ var getWeatherData = function () {
   console.log(searchFieldEl.value);
 };
 
+// Fetches 5 Day Forecast from openweathermap.org
 var fiveDayForecast = function () {
   var apiUrl =
     'https://api.openweathermap.org/data/2.5/forecast?q=' +
@@ -81,6 +84,7 @@ var fiveDayForecast = function () {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
+        // 5 Day Weather Card 0
         weatherCardEl0.innerHTML = data.list[2].dt_txt;
         cardTempEl0.innerHTML =
           'Temp: ' + Math.round(data.list[2].main.temp) + ' F';
@@ -89,6 +93,7 @@ var fiveDayForecast = function () {
         cardHumidityEl0.innerHTML = data.list[2].main.humidity + '%';
         cardWeatherEl0.innerHTML = data.list[2].weather[0].description;
 
+        // 5 Day Weather Card 1
         weatherCardEl1.innerHTML = data.list[10].dt_txt;
         cardTempEl1.innerHTML =
           'Temp: ' + Math.round(data.list[10].main.temp) + ' F';
@@ -97,6 +102,7 @@ var fiveDayForecast = function () {
         cardHumidityEl1.innerHTML = data.list[10].main.humidity + '%';
         cardWeatherEl1.innerHTML = data.list[10].weather[0].description;
 
+        // 5 Day Weather Card 2
         weatherCardEl2.innerHTML = data.list[18].dt_txt;
         cardTempEl2.innerHTML =
           'Temp: ' + Math.round(data.list[18].main.temp) + ' F';
@@ -105,6 +111,7 @@ var fiveDayForecast = function () {
         cardHumidityEl2.innerHTML = data.list[18].main.humidity + '%';
         cardWeatherEl2.innerHTML = data.list[18].weather[0].description;
 
+        // 5 Day Weather Card 3
         weatherCardEl3.innerHTML = data.list[26].dt_txt;
         cardTempEl3.innerHTML =
           'Temp: ' + Math.round(data.list[26].main.temp) + ' F';
@@ -113,6 +120,7 @@ var fiveDayForecast = function () {
         cardHumidityEl3.innerHTML = data.list[26].main.humidity + '%';
         cardWeatherEl3.innerHTML = data.list[26].weather[0].description;
 
+        // 5 Day Weather Card 4
         weatherCardEl4.innerHTML = data.list[34].dt_txt;
         cardTempEl4.innerHTML =
           'Temp: ' + Math.round(data.list[34].main.temp) + ' F';
